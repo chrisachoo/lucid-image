af78b60e77de986c227535a93b499aa91447754b
chrisachoo <mabs.chris@outlook.com>
2025-06-30 23:39:19 +0200

    Remove getImageSizeOnServer export and update tsup config for external dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { LucidImage } from "./lucid-image"
 export type { LucidImageProps } from "./lucid-image"
 export { getImage } from "./utils/get-image"
-export { getImageSizeOnServer } from "./utils/get-image-size.server"
