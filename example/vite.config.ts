755bd9746873729a67f0e28a0bcfe0eceb15ef9d
chrisachoo <mabs.chris@outlook.com>
2025-06-30 22:14:27 +0200

    Implement CI workflow and remove legacy deploy workflow

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,14 +1,17 @@
-import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import path from "node:path"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
+	base: '/lucid-image/',
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			"lucid-image": path.resolve(__dirname, "../src") // Local dev
 		}
+	},
+	build: {
+		outDir: 'dist',
 	}
 })
